9d0996ef49153356f708fb981921bab4bedc0e7a
Tiffany <tchan12899@gmail.com>
2024-01-30 21:58:41 -0500

    Initialized category model + basic GET route

--- routes/api/category-routes.js
+++ routes/api/category-routes.js
@@ -6,6 +6,10 @@ const { Category, Product } = require('../../models');
 router.get('/', (req, res) => {
   // find all categories
   // be sure to include its associated Products
+  Category.findAll()
+  .then((category) => {
+  res.json(category);
+  });
 });
 
 router.get('/:id', (req, res) => {

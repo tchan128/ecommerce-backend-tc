8add5d4bd1b19d75ae6896c1013581aeec4b6e7e
Tiffany <tchan12899@gmail.com>
2024-01-31 19:39:15 -0500

    Completed PUT DELETE POST routes for tags

--- routes/api/tag-routes.js
+++ routes/api/tag-routes.js
@@ -21,18 +21,55 @@ router.get('/', (req, res) => {
 router.get('/:id', (req, res) => {
   // find a single tag by its `id`
   // be sure to include its associated Product data
+  Tag.findAll({
+    include: [
+      {
+        model: Product
+      }
+    ], 
+    where: {
+      id: req.params.id
+    }
+  })
+  .then((product) => {
+    res.json(product)
+  })
 });
 
 router.post('/', (req, res) => {
   // create a new tag
+  Tag.create({
+    tag_name: req.body.tag_name
+  })
+  .then((tag) => {
+    res.json(tag)
+  })
 });
 
 router.put('/:id', (req, res) => {
   // update a tag's name by its `id` value
+  Tag.update({
+    tag_name: req.body.tag_name
+  },
+  {where: {
+    id: req.params.id
+  }
+  })
+  .then((tag) => {
+    res.json(tag)
+  })
 });
 
 router.delete('/:id', (req, res) => {
   // delete on tag by its `id` value
+  Tag.destroy({
+    where: {
+      id: req.params.id
+    }
+  })
+  .then((tag) => {
+    res.json(tag)
+  })
 });
 
 module.exports = router;
